997b51fe45d1950ca3931807d1a30362fd82c8e4
netotz <neto.otz@hotmail.com>
2021-09-29 16:55:28 -0500

    Don't use pandas

--- main.py
+++ main.py
@@ -1,41 +1,55 @@
 import asyncio
 
 from colorama import init, Fore, Style
-import pandas as pd
+import openpyxl as opx
 import aiohttp
 
 
 async def check_uptime(path: str) -> None:
     print('Leyendo Excel...\n')
-    excel = pd.read_excel(
-        path,
-        engine='openpyxl',
-        header=6,
-        usecols=[5, 6, 7, 9]
+
+    red_fill = opx.styles.PatternFill(
+        fill_type='solid',
+        fgColor=opx.styles.colors.Color('50FF0000')
+    )
+    green_fill = opx.styles.PatternFill(
+        fill_type='solid',
+        fgColor=opx.styles.colors.Color('5000979700')
     )
 
-    statuses = list()
+    excel = opx.load_workbook(path)
+    sheet = excel.active
+    sheet.insert_cols(11)
     session = aiohttp.ClientSession()
-    for index, row in excel.iterrows():
-        name = ' '.join(str(row[i]) for i in range(5, 8, 1))
+    i = 0
+    for row in sheet.iter_rows(min_row=8, min_col=6, max_col=11):
+        if i == 10:
+            break
+        i += 1
+        name = ' '.join(str(row[i].value) for i in range(0, 3, 1))
         if not name.isupper():
             name = 'Clasificado'
-        url = row[9]
+
+        url = row[4].value
 
         print(f'Contrato de {Style.BRIGHT}{name}{Style.RESET_ALL}')
         print(f'Checando enlace {Fore.CYAN}{url}{Fore.RESET}...')
 
         async with session.head(url) as response:
             status = response.status
-            color = Fore.GREEN if 200 <= status < 300 else Fore.RED
+            is_success = 200 <= status < 300
+            color = Fore.GREEN if is_success else Fore.RED
             print(f'Código de estado: {color}{status}{Fore.RESET}\n')
-            statuses.append(status)
+            row[5].value = status
+
+            fill = green_fill if is_success else red_fill
+            row[4].fill = fill
+            row[5].fill = fill
+
     await session.close()
 
-    excel.insert(10, 'Estado del hipervínculo al contrato', statuses)
-
-    print('Guardando Excel...')
-    excel.to_excel('estados.xlsx', engine='openpyxl')
+    print('Guardando estados de código...')
+    excel.save('estados.xlsx')
 
 
 if __name__ == '__main__':
